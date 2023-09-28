9f0ec865274459fa91f06c7f8664efda4d40ef0b
Stef Pletinck <s@stefpletinck.be>
2023-09-28 16:24:59 +0200

    Further tests

--- BeluxPluginTest/BeluxPluginTest.cpp
+++ BeluxPluginTest/BeluxPluginTest.cpp
@@ -19,6 +19,13 @@ namespace BeluxPluginTest
 			return allocation_file;
 		}
 
+		static SidAllocation get_filled_allocator()
+		{
+			const SidAllocation allocation;
+			allocation.parse_string(get_allocation_file());
+			return allocation;
+		}
+
 	public:
 		TEST_METHOD(TestParse)
 		{
@@ -41,11 +48,30 @@ namespace BeluxPluginTest
 			fake_now.tm_mday = 25;
 			fake_now.tm_mon = 8; // September
 			fake_now.tm_year = 2023;
+			fake_now.tm_hour = 22; // Late, to perhaps trigger the four-engine case.
 
-			const auto maybe_sid = allocation.find("EBBR", "CIV", "EDKH",
+			const auto maybe_sid = allocation.find("EBBR", "CIV", "EKCH",
 			                                       2, "25R", fake_now);
 			Assert::IsTrue(maybe_sid.has_value());
 			Assert::AreNotEqual(std::string("CIV2D"), maybe_sid.value().sid);
+		}
+
+		TEST_METHOD(AssignsCIV2DForFourEngineOnWeekdays)
+		{
+			const auto allocator = get_filled_allocator();
+			tm fake_now{};
+			fake_now.tm_wday = 1; // Monday
+			fake_now.tm_mday = 25;
+			fake_now.tm_mon = 8; // September
+			fake_now.tm_year = 2023;
+			fake_now.tm_hour = 22; // Late, to perhaps trigger the four-engine case.
+
+			const auto maybe_sid = allocator.find("EBBR", "CIV", "EKCH",
+				4, "25R", fake_now);
+
+			Assert::IsTrue(maybe_sid.has_value());
+			Assert::AreEqual(std::string("CIV2D"), maybe_sid->sid);
+
 		}
 	};
 }
