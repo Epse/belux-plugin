b249066d03199b07fb05c9a6d78f2f1120f6bfc7
Stef Pletinck <s@stefpletinck.be>
2024-06-05 20:04:59 +0200

    Fix Gate Planner issues
    Gate changes are now correctly detected again,
    and the parsing properly handles the format.

--- BeluxPlugin/BeluxGatePlanner.cpp
+++ BeluxPlugin/BeluxGatePlanner.cpp
@@ -19,23 +19,28 @@ void BeluxGatePlanner::parse_json(std::string const& data) {
 
 	// I use the throwing version of all this, then just set the gate list empty if something goes wrong..
 	try {
-		const auto document = parse(data).as_array();
-		for (const value& airport_entry : document)
+		const auto parsed = parse(data);
+		const auto document = parsed.as_array().at(0).as_object();
+		for (const auto& airport_entry : document)
 		{
-			const auto airport = std::string(airport_entry.as_object().begin()->key());
-			const auto entries = airport_entry.as_object().at(airport).as_object();
+			const auto airport = std::string(airport_entry.key());
+			const auto entries = airport_entry.value().as_object();
 			for (const auto& entry : entries)
 			{
-				temp_gate_list.insert_or_assign(std::string(entry.key()),
-					BeluxGateEntry(
-						std::string(entry.key()),
-						airport,
-						std::string(entry.value().as_string())));
+				const std::string callsign = entry.key();
+				const std::string gate = std::string(entry.value().as_string());
+				BeluxGateEntry gate_entry(callsign, airport, gate);
+				if (this->gate_list.find(callsign) != this->gate_list.end())
+				{
+					gate_entry.gate_has_changed = this->gate_list.at(callsign).gate == gate;
+				}
+				temp_gate_list.insert_or_assign(std::string(entry.key()), gate_entry);
 			}
 		}
 	} catch (std::exception const& e)
 	{
 		// Should probably log this, but we'll just ignore at the moment
+		const auto copy = e.what();
 	}
 
     this->gate_list = temp_gate_list;
